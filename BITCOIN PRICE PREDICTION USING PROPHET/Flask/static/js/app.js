12bb1603fa0200a9fd2c71e5c789beefc0c3e0a5
Pranaykumar30 <141347187+Pranaykumar30@users.noreply.github.com>
2024-11-17 16:39:04 +0530

    Delete BITCOIN PRICE PREDICTION USING PROPHET/Flask directory

--- BITCOIN PRICE PREDICTION USING PROPHET/Flask/static/js/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("predictionForm").addEventListener("submit", function(event) {
-    var dateInput = document.getElementById("dateInput").value;
-
-    if (dateInput === "") {
-        alert("Please select a date for prediction.");
-        event.preventDefault();
-    }
-});
